02afe42b0128eaa99694db4abf205f80f7bee344
premnadh <premnathbr123@gmail.com>
2023-10-16 22:47:34 +0530

    landing page

--- lib/app/view/Landingpage/landingpage.dart
+++ lib/app/view/Landingpage/landingpage.dart
@@ -15,6 +15,30 @@ class _LandingPageState extends State<LandingPage> {
         centerTitle: true,
         title: const Text('Landing Page'),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to Our App!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Explore the amazing features we offer.',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                // Add your authentication logic here
+              },
+              child: Text('Login with Google'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
